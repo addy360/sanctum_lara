69a93e2e99f5ab27c2c2826181d1c14ed4cefbdf
Addy360 <abbyboy360@gmail.com>
2021-01-12 12:20:47 +0300

    scaffoolding app for testing sanctum

---
+++ resources/js/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <h4>App component</h4>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
